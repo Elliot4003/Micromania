d6766cf09a829e1e571455417119f727905f91c5
elliot.crozet <elliot.crozet@groupeadequat.fr>
2024-08-27 15:34:06 +0200

    first commit

---
+++ Micromania/Article.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micromania
+{
+    internal class Article
+    {
+        private static int numCounter;
+        private int numRef;
+        public string name;
+        public double price;
+        private static int quantity;
+
+        public Article(string name, double price)
+        {
+            Name = name;
+            Price = price;
+            NumCounter++;
+            NumRef = NumCounter;
+            Quantity++;
+        }
+
+        public static int NumCounter
+        {
+            get { return numCounter; }
+            private set { numCounter = value; }
+        }
+
+        public int NumRef
+        {
+            get { return numRef; }
+            private set { numRef = value; }
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public double Price
+        {
+            get;
+            set;
+        }
+
+        public static int Quantity
+        {
+            get { return quantity; }
+            private set { quantity = value; }
+        }
+
+        public static void PrintArticles(List<Article> articlesList)
+        {
+            Console.WriteLine("\n" + Article.Quantity.ToString() + " articles en stock.\n");
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            foreach (Article article in articlesList)
+            {
+                string articleFormat = "Ref : " + article.NumRef + "\n" +
+                                       "Nom : " + article.Name + "\n" +
+                                       "Prix : " + article.Price + "\n";
+
+                Console.WriteLine("\n");
+                Console.WriteLine(articleFormat);
+            }
+
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            // Commencer en haut de la liste
+            Console.SetCursorPosition(0, 0);
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void SearchByRef(List<Article> articlesList, string numRefStr)
+        {
+            if (Int32.TryParse(numRefStr, out int numRef))
+            {
+                bool result = false;
+                foreach (Article article in articlesList)
+                {
+                    if (article.NumRef == numRef)
+                    {
+                        string articleFormat = "Article trouvé en stock : \n\n" +
+                                               "Ref : " + article.NumRef + "\n" +
+                                               "Nom : " + article.Name + "\n" +
+                                               "Prix : " + article.Price + "\n";
+
+                        Console.WriteLine(articleFormat);
+                        result = true;
+                        break;
+                    }
+                }
+                if (result == false) Console.WriteLine("Aucun résultat. \n");
+            } else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Le numéro de référence est composé uniquement de chiffres. \n");
+                Console.ResetColor();
+            }
+            
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void SearchByName(List<Article> articlesList, string name)
+        {
+            if (name == "") Console.WriteLine("Aucun résultat. \n");
+            else
+            {
+                name = name.ToLower();
+
+                var queryName = from a in articlesList
+                                where a.Name.ToLower().Contains(name)
+                                select a;
+
+                if (queryName.Count() > 0)
+                {
+                    string foundString;
+                    if (queryName.Count() == 1) foundString = $"1 article trouvé en stock : \n";
+                    else foundString = $"{queryName.Count()} articles trouvés en stock : \n";
+                    Console.WriteLine(foundString);
+
+                    foreach (Article a in queryName)
+                    {
+                        string articleFormat = "Ref : " + a.NumRef + "\n" +
+                                               "Nom : " + a.Name + "\n" +
+                                               "Prix : " + a.Price + "\n";
+
+                        Console.WriteLine(articleFormat);
+                    }
+
+                }
+
+                else Console.WriteLine("Aucun résultat. \n");
+            }
+            
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void SearchByPrice(List<Article> articlesList, string priceMin, string priceMax)
+        {
+            if (Int32.TryParse(priceMin, out int lowerPrice) && Int32.TryParse(priceMax, out int upperPrice))
+            {
+                var queryPrice = from a in articlesList
+                                 where a.Price > lowerPrice && a.Price < upperPrice
+                                 select a;
+
+                if (queryPrice.Count() > 0)
+                {
+                    string foundString;
+                    if (queryPrice.Count() == 1) foundString = $"1 article trouvé en stock : \n";
+                    else foundString = $"{queryPrice.Count()} articles trouvés en stock : \n";
+                    Console.WriteLine(foundString);
+                    Console.WriteLine("\nAppuyez sur la barre espace pour quitter...\n");
+
+                    foreach (Article a in queryPrice)
+                    {
+                        string articleFormat = "Ref : " + a.NumRef + "\n" +
+                                               "Nom : " + a.Name + "\n" +
+                                               "Prix : " + a.Price + "\n";
+
+                        Console.WriteLine(articleFormat);
+                    }
+
+                }
+                else Console.WriteLine("Aucun résultat. \n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Le prix ne peut être composé que de chiffres \n");
+                Console.ResetColor();
+            }
+
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            // Commencer en haut de la liste
+            Console.SetCursorPosition(0, 0);
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void RemoveByRef(List<Article> articlesList, string numRefStr)
+        {
+            if (Int32.TryParse(numRefStr, out int numRef))
+            {
+                for (int i = 0; i < articlesList.Count; i++)
+                {
+                    if (articlesList[i].NumRef == numRef)
+                    {
+                        // Afficher les informations
+                        string articleFormat = "Ref : " + articlesList[i].NumRef + "\n" +
+                                               "Nom : " + articlesList[i].Name + "\n" +
+                                               "Prix : " + articlesList[i].Price + "\n";
+                        Console.WriteLine("Article à supprimer : \n");
+                        Console.WriteLine(articleFormat + "\n");
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Voulez-vous supprimer l'article? (O/N) ");
+                        Console.ResetColor();
+
+                        string deleteArticle = Console.ReadLine();
+
+                        if (deleteArticle == "O" || deleteArticle == "o")
+                        {
+                            Console.WriteLine("\nL'article '" + articlesList[i].Name + "' a été supprimé. \n");
+                            articlesList.RemoveAt(i);
+                            Article.Quantity--;
+                            break;
+                        }
+                        else break;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Le numéro de référence est composé uniquement de chiffres. \n");
+                Console.ResetColor();
+            }
+
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void UpdateByRef(List<Article> articlesList, string numRefStr)
+        {
+            if (Int32.TryParse(numRefStr, out int numRef))
+            {
+                foreach (Article article in articlesList)
+                {
+                    if (article.NumRef == numRef)
+                    {
+                        // Afficher les informations
+                        string articleFormat = "Ref : " + article.NumRef + "\n" +
+                                               "Nom : " + article.Name + "\n" +
+                                               "Prix : " + article.Price + "\n";
+                        Console.WriteLine("Article à modifier : \n");
+                        Console.WriteLine(articleFormat + "\n");
+
+                        string name = article.Name;
+                        bool isUpdatedName = false;
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("Modifier le nom ? (O/N) ");
+                        Console.ResetColor();
+                        string nameUpdate = Console.ReadLine();
+
+                        while (isUpdatedName == false)
+                        {
+                            if (nameUpdate == "O" || nameUpdate == "o")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write("\nNouveau nom : ");
+                                Console.ResetColor();
+                                name = Console.ReadLine();
+                            }
+                            else break;
+                            if (name == "")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nL'article doit avoir un nom.");
+                                Console.ResetColor();
+                            }
+                            else isUpdatedName = true;
+                        }
+                        article.Name = name;
+
+                        string price = article.Price.ToString();
+                        bool isUpdatedPrice = false;
+                        bool isDigitsOnly = true;
+
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("\nModifier le prix ? (O/N) ");
+                        Console.ResetColor();
+                        string priceUpdate = Console.ReadLine();
+
+                        while (isUpdatedPrice == false)
+                        {
+                            if (priceUpdate == "O" || priceUpdate == "o")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write("\nNouveau prix : ");
+                                Console.ResetColor();
+                                price = Console.ReadLine();
+                            }
+                            else break;
+
+                            foreach (char c in price)
+                            {
+                                if (c < '0' || c > '9')
+                                {
+                                    if (!(c == '.' || c == ','))
+                                    {
+                                        isDigitsOnly = false;
+                                        break;
+                                    }
+                                }
+                                else isDigitsOnly = true;
+                            }
+
+                            if (price == "")
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nL'article doit avoir un prix.");
+                                Console.ResetColor();
+                            }
+                            else if (isDigitsOnly == false)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nLe prix ne peut être composé que de chiffres.");
+                                Console.ResetColor();
+                            }
+                            else if (price.Contains('-'))
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("\nLe prix ne peut pas être négatif.");
+                                Console.ResetColor();
+                            }
+                            else isUpdatedPrice = true;
+                        }
+                        price = price.Replace('.', ',');
+                        double priceFinal = Convert.ToDouble(price);
+                        article.Price = priceFinal;
+
+                        // Afficher les modifications
+                        string articleFormatFinal = "Ref : " + article.NumRef + "\n" +
+                                               "Nom : " + article.Name + "\n" +
+                                               "Prix : " + article.Price + "\n";
+
+                        Console.WriteLine("\n" + articleFormatFinal + "\n");
+
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Le numéro de référence est composé uniquement de chiffres. \n");
+                Console.ResetColor();
+            }
+
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+
+        public static void AddArticle(List<Article> articlesList, string name, string priceStr)
+        {
+            bool isDigitsOnly = true;
+            foreach (char c in priceStr)
+            {
+                if (c < '0' || c > '9')
+                {
+                    if (!(c == '.' || c == ','))
+                    {
+                        isDigitsOnly = false;
+                        break;
+                    }
+                }
+                else isDigitsOnly = true;
+            }
+            if (name == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nL'article doit avoir un nom.");
+                Console.ResetColor();
+            }
+            else if (priceStr == "")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nL'article doit avoir un prix.");
+                Console.ResetColor();
+            }
+            else if (isDigitsOnly == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nLe prix ne peut être composé que de chiffres.");
+                Console.ResetColor();
+            }
+            else if (priceStr.Contains('-'))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nLe prix ne peut pas être négatif.");
+                Console.ResetColor();
+            }
+            else 
+            {
+                priceStr = priceStr.Replace('.', ',');
+                if (Double.TryParse(priceStr, out double price))
+                {
+
+                    articlesList.Add(new Article(name, price));
+
+                    for (int i = 0; i < Article.Quantity; i++)
+                    {
+                        if (i > Article.Quantity - 2)
+                        {
+                            string articleFormatFinal = "Ref : " + articlesList[i].NumRef + "\n" +
+                                                   "Nom : " + articlesList[i].Name + "\n" +
+                                                   "Prix : " + articlesList[i].Price + "\n";
+
+                            Console.WriteLine("\n" + articleFormatFinal + "\n");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Le prix ne peut être composé que de chiffres. \n");
+                    Console.ResetColor();
+                }
+            }
+
+            // Attendre la confirmation de l'utilisateur
+            Console.Write("\nAppuyez sur la barre espace pour quitter...");
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+            Console.Clear();
+        }
+    }
+}
---
+++ Micromania/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Micromania
+{
+    internal class Program
+    {
+        // On peut imaginer ici un appel de la bdd
+        public static List<Article> articlesList = new List<Article> { 
+            new Article("Ratchet & Clank: Rift Apart", 59.99), 
+            new Article("The Last of Us Part I", 59.99), 
+            new Article("God of War: Ragnarök", 59.99), 
+            new Article("Uncharted 4", 59.99), 
+            new Article("Spider-Man 2", 69.99), 
+            new Article("Kingdom Hearts II", 49.99), 
+            new Article("Resident Evil 4", 29.99),
+            new Article("Far Cry 3", 24.99),
+            new Article("Minecraft", 29.99),
+            new Article("The Legend of Zelda: Breath of the Wild", 39.99),
+            new Article("Elden Ring", 59.99),
+            new Article("Luigi's Mansion 3", 39.99),
+            new Article("Animal Crossing: New Horizons", 39.99),
+            new Article("Grand Theft Auto V", 49.99),
+            new Article("Star Wars Battlefront II", 29.99)
+        };
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            do
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("        *******************************************************************************************************");
+                Console.WriteLine("        *   ___      ___    _    ______    _____    _____    ___      ___    _____    ___   _    _    _____   *");
+                Console.WriteLine("        *  |   \\    /   |  | |  | _____|  |  _  |  |  _  |  |   \\    /   |  |  _  |  |   \\ | |  | |  |  _  |  *");
+                Console.WriteLine("        *  | |\\ \\  / /| |  | |  | |       | |_| |  | | | |  | |\\ \\  / /| |  | |_| |  | |\\ \\| |  | |  | |_| |  *");
+                Console.WriteLine("        *  | | \\ \\/ / | |  | |  | |       |    _|  | | | |  | | \\ \\/ / | |  |  _  |  | | \\ \\ |  | |  |  _  |  *");
+                Console.WriteLine("        *  | |  \\__/  | |  | |  | |____   | |\\ \\   | |_| |  | |  \\__/  | |  | | | |  | |  \\  |  | |  | | | |  *");
+                Console.WriteLine("        *  |_|        |_|  |_|  |______|  |_| \\_\\  |_____|  |_|        |_|  |_| |_|  |_|   \\_|  |_|  |_| |_|  *");
+                Console.WriteLine("        *                                                                                                     *");
+                Console.WriteLine("        *******************************************************************************************************");
+                Console.WriteLine("");
+
+                // Ajout
+                Console.WriteLine("\n  1 - Ajout");
+
+                // Suppression
+                Console.WriteLine("\n  2 - Suppression");
+
+                // Update
+                Console.WriteLine("\n  3 - Modification");
+
+                // Listing
+                Console.WriteLine("\n  4 - Liste des articles");
+
+                // Recherche par ref
+                Console.WriteLine("\n  5 - Recherche par référence");
+
+                // Recherche par nom
+                Console.WriteLine("\n  6 - Recherche par nom");
+
+                // Recherche par prix
+                Console.WriteLine("\n  7 - Recherche par prix");
+
+                // Quitter l'application
+                Console.WriteLine("\n  8 - Quitter");
+
+                Console.Write("\n");
+
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.NumPad1:
+                        Console.Clear();
+                        AddArticle();
+                        break;
+                    case ConsoleKey.NumPad2:
+                        Console.Clear();
+                        RemoveByRef();
+                        break;
+                    case ConsoleKey.NumPad3:
+                        Console.Clear();
+                        UpdateByRef();
+                        break;
+                    case ConsoleKey.NumPad4:
+                        Console.Clear();
+                        PrintArticles();
+                        break;
+                    case ConsoleKey.NumPad5:
+                        Console.Clear();
+                        SearchByRef();
+                        break;
+                    case ConsoleKey.NumPad6:
+                        Console.Clear();
+                        SearchByName();
+                        break;
+                    case ConsoleKey.NumPad7:
+                        Console.Clear();
+                        SearchByPrice();
+                        break;
+                    case ConsoleKey.NumPad8:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+
+                }
+            } while (true);
+
+        }
+
+        static void PrintArticles()
+        {
+            Article.PrintArticles(articlesList);
+        }
+
+        static void AddArticle()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  -------------------");
+            Console.WriteLine("  | AJOUT D'ARTICLE |");
+            Console.WriteLine("  -------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez le nom de l'article : ");
+            Console.ResetColor();
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez le prix de l'article : ");
+            Console.ResetColor();
+
+            string price = Console.ReadLine();
+
+
+            Console.WriteLine("\n");
+
+            Article.AddArticle(articlesList, name, price);
+        }
+
+        static void UpdateByRef()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  --------------------------");
+            Console.WriteLine("  | MODIFICATION D'ARTICLE |");
+            Console.WriteLine("  --------------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un numéro de référence : ");
+            Console.ResetColor();
+
+            string numRef = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Article.UpdateByRef(articlesList, numRef);
+
+        }
+
+        static void RemoveByRef()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  -------------------------");
+            Console.WriteLine("  | SUPPRESSION D'ARTICLE |");
+            Console.WriteLine("  -------------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un numéro de référence : ");
+            Console.ResetColor();
+
+            string numRef = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Article.RemoveByRef(articlesList, numRef);
+
+        }
+
+        static void SearchByRef()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  ---------------------------");
+            Console.WriteLine("  | RECHERCHE PAR REFERENCE |");
+            Console.WriteLine("  ---------------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un numéro de référence : ");
+            Console.ResetColor();
+            
+            string numRef = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Article.SearchByRef(articlesList, numRef);
+
+        }
+
+        static void SearchByName()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  ---------------------");
+            Console.WriteLine("  | RECHERCHE PAR NOM |");
+            Console.WriteLine("  ---------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un nom d'article : ");
+            Console.ResetColor();
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Article.SearchByName(articlesList, name);
+
+        }
+
+        static void SearchByPrice()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n  ----------------------");
+            Console.WriteLine("  | RECHERCHE PAR PRIX |");
+            Console.WriteLine("  ----------------------\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un prix minimum : ");
+            Console.ResetColor();
+
+            string priceMin = Console.ReadLine();
+
+            Console.WriteLine("\n");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Entrez un prix maximum : ");
+            Console.ResetColor();
+
+            string priceMax = Console.ReadLine();
+
+
+            Console.WriteLine("\n");
+
+            Article.SearchByPrice(articlesList, priceMin, priceMax);
+        }
+    }
+}
